58ae93e02799edd3daab673b219a3251f21f79d1
Safakylmz <safakyilmaz8@hotmail.com>
2022-10-05 17:15:16 +0300

    end of the tutorial

---
+++ Jelly-Shift-Tutorial/Assets/Scripts/GhostJelly.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostJelly : MonoBehaviour
+{
+    public static GhostJelly instance;
+    public Transform[] ghostPoses;
+
+    private int index = 0;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void ChangeScaleOfTheGhost(Vector3 scale)
+    {
+        scale.y *= 1.5f;
+        transform.localScale = scale;
+    }
+
+    public void ChangePosOfTheGhost()
+    {
+        index++;
+        if(ghostPoses.Length > index)
+        {
+            transform.position = ghostPoses[index].transform.position;
+        }
+    }
+}
